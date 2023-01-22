dddf07ee9b9bd7895b3c4f76da06c7108f23ec6a
HectorNapier <123218050+HectorNapier@users.noreply.github.com>
2023-01-22 16:39:51 -0600

    Add files via upload
    Upload Week 1 Homework 2315

---
+++ Homework1.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = (4*X*X) + (3*Y);
+        Console.WriteLine($"The value of Z is: {Z}");
+    }
+}
